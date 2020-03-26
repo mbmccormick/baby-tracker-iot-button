7f53afc9f0e11e611dd37880392ca217a82e6a16
Matt McCormick <matt@mbmccormick.com>
2020-03-26 09:33:44 -0700

    Fixing bug in uuidv4 library usage.

--- babyTracker.js
+++ babyTracker.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const moment = require("moment-timezone");
-import { v4 as uuidv4 } from "uuid";
+const { v4: uuidv4 } = require("uuid");
 
 const DeviceName = "Node.js";
 const DeviceOSInfo = "Node.js 1.0.0";
